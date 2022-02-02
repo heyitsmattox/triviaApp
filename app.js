74ab67994b56355c5667a253086a6c0a9b1bafbc
Matt Mattox <bizofmatt@gmail.com>
2022-02-01 20:25:03 -0800

    added card inner HTML

--- app.js
+++ app.js
@@ -14,12 +14,28 @@ function addGenre() {
   levels.forEach((level) => {
     const card = document.createElement('div')
     card.classList.add('card')
+    column.append(card)
+
+    if (level === 'easy') {
+        card.innerHTML = 100
+    }
+    if (level === 'medium') {
+        card.innerHTML = 200
+    }
+    if (level === 'hard') {
+        card.innerHTML = 300
+    }
 
     fetch(
       `https://opentdb.com/api.php?amount=10&category=11&difficulty=${level}&type=multiple`
     )
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => { 
+        console.log(data)
+        card.setAttribute('data-question', data.results[0].question)
+        card.setAttribute('data-answer', data.results[0].correct_answer)
+
+    })
 
   })
 
