624ce9afc39706955ee4e35d9cb401cd84c89f53
Matt Mattox <bizofmatt@gmail.com>
2022-02-01 19:43:02 -0800

    added some styles and javascript

---
+++ app.js
@@ -0,0 +1,11 @@
+const game = document.querySelector('#game');
+const score = document.querySelector('#score');
+
+const film = 11
+const levels = ['easy', 'medium', 'hard']
+
+function addGenre() {
+    const  column = document.createElement('div')
+    column.classList.add('genre-column')
+    
+}
