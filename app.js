8d35ee9199c32f055ddc00b20445b6e0ecc8d71b
Matt Mattox <bizofmatt@gmail.com>
2022-02-01 19:53:30 -0800

    added foreach and appended the div colum w/ id of game

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const game = document.querySelector('#game');
-const score = document.querySelector('#score');
+const game = document.querySelector("#game");
+const score = document.querySelector("#score");
 
-const film = 11
-const levels = ['easy', 'medium', 'hard']
+const film = 11;
+const levels = ["easy", "medium", "hard"];
 
 function addGenre() {
-    const  column = document.createElement('div')
-    column.classList.add('genre-column')
-    
+  const column = document.createElement("div");
+  column.classList.add("genre-column");
+  column.innerHTML = "this is a genre";
+  //Apend is a JS method -- append puts the column inside the id of "game"
+  game.append(column);
+
+  levels.forEach((level) => {
+    fetch(
+      `https://opentdb.com/api.php?amount=10&category=11&difficulty=${level}&type=multiple`
+    );
+  });
 }
+
+addGenre();
